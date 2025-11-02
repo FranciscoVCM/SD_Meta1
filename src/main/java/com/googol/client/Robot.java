61816eecc994b5df9c9685a8ebe69d4b055eb1db
Francisco Cid <francisco.cid.martins@gmail.com>
2025-11-02 17:26:36 +0000

    apaguei ficheiros useless em utf8

--- src/main/java/com/googol/client/Robot.java
+++
@@ -1,7 +0,0 @@
-package com.googol.client;
-
-public class Robot {
-    public static void main(String[] args) {
-        // TODO: cliente automático p/ testes de carga
-    }
-}
--- src/main/java/com/googol/index/StopWordsLearner.java
+++
@@ -1,7 +0,0 @@
-package com.googol.index;
-
-import java.util.Set;
-
-public class StopWordsLearner {
-    public Set<String> current() { return Set.of(); } // TODO: implementação distribuída (grupo 3)
-}
